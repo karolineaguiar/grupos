4c05da07d4234b40c7f664f8ac6c54194cdba5c7
Karoline Aguiar <karolinetrab@gmail.com>
2022-04-03 15:14:06 -0300

    atualizado os cards da dashboard

--- index.php
+++ index.php
@@ -18,23 +18,24 @@
     );
 
     ?>
-
-<div class="row">
     <h1> O objetivo de ser criado a contagem de empresas e colaboradores 
     é para uma melhor compreensão da amplitude empresarial presente. <br>
     Quaisquer dúvidas, pressione no menu colaboradores e em seguida clique na palavra dúvidas.<br>
     Boa sorte!!
     </h1>
-    <div class="col-6">
-        <div class="card m-5 p-5"> 
+
+<div class="row">
+    
+    <div class="col-6" align="center">
+        <div class="card m-5 card-personalizado"> 
             Empresas
-            <p><?php echo $quantidadeEmpresas; ?></p>
+            <p style="font-size:50px"><?php echo $quantidadeEmpresas; ?></p>
         </div>
     </div>
-    <div class="col-6">
-        <div class="card m-5 p-5">
+    <div class="col-6" align="center">
+        <div class="card m-5 card-personalizado">
             Colaboradores
-            <p><?php echo $quantidadeColaboradores; ?></p>
+            <p style="font-size:50px"><?php echo $quantidadeColaboradores; ?></p>
         </div>
     </div>
 </div>
